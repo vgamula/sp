53cd23c72cd35192c5c4188b5f83a0a82523eec3
Volodymyr Gamula <v.gamula@gmail.com>
2016-12-08 01:09:16 +0200

    Use explicit loop

--- server/main.py
+++ server/main.py
@@ -7,13 +7,14 @@ async def handle(request: web.Request):
     return web.Response(text='Hello, World!')
 
 
-def make_app() -> web.Application:
-    app = web.Application()
+def make_app(loop: asyncio.AbstractEventLoop) -> web.Application:
+    app = web.Application(loop=loop)
     app.router.add_get('/', handle)
     return app
 
 
 if __name__ == '__main__':
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    app = make_app()
+    loop = asyncio.get_event_loop()
+    app = make_app(loop=loop)
     web.run_app(app, port=8888)
