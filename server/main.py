d4889dffd80e10132c092c58bab324ff7a9a41da
Volodymyr Gamula <v.gamula@gmail.com>
2016-12-08 15:19:31 +0200

    Remove debugger

--- server/main.py
+++ server/main.py
@@ -7,8 +7,10 @@ from aiohttp import web
 from server.core.config_utils import load_config
 from server.routes import make_routes
 
+
 PROJECT_ROOT = pathlib.Path(__file__).parent.parent
 SERVER_ROOT = PROJECT_ROOT / 'server'
+
 
 async def handle(request: web.Request):
     return web.Response(text='Hello, World!')
@@ -27,5 +29,4 @@ if __name__ == '__main__':
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
     app = make_app(loop=loop)
-    import ipdb; ipdb.set_trace()
     web.run_app(app, port=8888)
