54793831c020d416547494c2150ae5037c66d1cc
Volodymyr Gamula <v.gamula@gmail.com>
2016-12-21 18:42:32 +0200

    Improve settings

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -3,37 +3,23 @@ from pathlib import Path
 
 get = os.environ.get
 
-
 DEBUG = False
 ENVIRONMENT = get('ENV', 'production')  # development, testing
 DATABASE_URL = get('DATABASE_URL', 'mongodb://localhost:27017')
 SECRET_KEY = get('SECRET_KEY', '109la0m3tK8ErcOJGJNqkQTU-KdvEqw8oEnfKZ556LQ=')
-
-WEBPACK_CONFIG = {
-    'DEFAULT': {
-        'CACHE': False,
-        'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'stats.json')
-    },
-    'VENDOR': {
-        'CACHE': False,
-        'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'vendor-stats.json')
-    }
-}
-
 
 try:
     from .local_settings import *
 except ImportError:
     pass
 
-if not DEBUG:
-    WEBPACK_CONFIG = {
-        'DEFAULT': {
-            'CACHE': True,
-            'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'stats.json')
-        },
-        'VENDOR': {
-            'CACHE': True,
-            'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'vendor-stats.json')
-        }
+WEBPACK_CONFIG = {
+    'DEFAULT': {
+        'CACHE': not DEBUG,
+        'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'stats.json')
+    },
+    'VENDOR': {
+        'CACHE': not DEBUG,
+        'STATS_FILE': str(Path(__file__).parent.parent / 'static' / 'dist' / 'vendor-stats.json')
     }
+}
